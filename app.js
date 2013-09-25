cb138c8be79ed12224abfa75950cde65cc3e0eb4
Pierre Cholhot <pierre.cholhot@digitas.fr>
2013-09-25 11:14:50 +0200

    Hello

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var express = require('express'),
   routes = require('./routes'),
   api = require('./routes/api'),
   http = require('http'),
-  path = require('path');
+  path = require('path'),
+  request = require('request');
 
 var app = module.exports = express();
 
@@ -17,7 +18,7 @@ var app = module.exports = express();
  */
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.logger('dev'));
@@ -42,7 +43,17 @@ if (app.get('env') === 'production') {
  */
 
 // serve index and view partials
-app.get('/', routes.index);
+app.get('/', function(req, res){
+  if(typeof(req.query._escaped_fragment_) !== "undefined") {
+    var r = request({
+      uri: 'http://localhost:3000/' + req.query._escaped_fragment_
+    });
+    r.pipe(res);
+  }else{
+    routes.index(req, res);
+  }
+});
+
 app.get('/partials/:name', routes.partials);
 
 // JSON API
@@ -55,6 +66,11 @@ app.get('*', routes.index);
 /**
  * Start Server
  */
+
+// http://fathomless-temple-9323.herokuapp.com/
+
+// RewriteCond %{QUERY_STRING} ^_escaped_fragment_=(.*)$
+// RewriteRule (.*) http://fathomless-temple-9323.herokuapp.com/%1? [P]
 
 http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
